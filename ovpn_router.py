91084619225b746bf9f3c0081c996d33f3db1e89
zhuchunmao <38986778+zhuchunmao@users.noreply.github.com>
2018-06-04 09:57:11 +0800

    Update ovpn_router.py

--- ovpn_router.py
+++ ovpn_router.py
@@ -14,7 +14,7 @@ for cidr in content:
     blocks.append(netaddr.IPNetwork(cidr.strip()))
 if len(blocks) == 0:
  sys.exit(1)
-file = open('/root/gfwlist/router', 'w')
+file = open('/root/router', 'w')
 file.truncate()
 for net in netaddr.cidr_merge(blocks):
  string = 'route ' + str(net.ip) + ' ' + str(net.netmask) + ' vpn_gateway\n'
